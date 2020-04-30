36c217ec31e5f7af9c47650fab43e0e4c5cb1c3d
AbdouAzizDIOUF <abdouazizdiouf75@gmail.com>
2020-05-01 01:05:24 +0200

    mon first commit et fin du projet

---
+++ angular-l1/src/app/etudiants/etudiants.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-etudiants',
+  templateUrl: './etudiants.component.html',
+  styleUrls: ['./etudiants.component.css']
+})
+export class EtudiantsComponent implements OnInit {
+
+  listFormations;
+  listEetudiants;
+  currentFormation={id:-1};
+
+  constructor(private httpClient:HttpClient) { }
+
+  ngOnInit() {
+      this.httpClient.get("http://localhost:8001/formations")
+        .subscribe(data=>{
+            this.listFormations=data;
+        },error => {
+          console.log(error)
+        });
+   }
+
+  onGetEtudiants(f){
+    this.currentFormation=f;
+    this.httpClient.get("http://localhost:8001/formations/"+f.id+"/etudiants")
+      .subscribe(data=>{
+        this.listEetudiants=data;
+      },error => {
+        console.log(error)
+      });
+  }
+
+}
